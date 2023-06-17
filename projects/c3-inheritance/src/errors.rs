b0f1e9125ba37cec9a7079f62b13db663fc75bad
Aster <galaster@foxmail.com>
2023-06-17 13:42:10 +0800

    Add the c3 inheritance algorithm

--- projects/c3-inheritance/src/errors.rs
+++ projects/c3-inheritance/src/errors.rs
@@ -1,6 +1,20 @@
+use std::fmt::{self, Display, Formatter};
+
 #[derive(Debug, Copy, Clone)]
 pub enum Error {
-    UnknownError
+    NotFound,
+    BadHead,
+    Circular,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Error::NotFound => write!(f, "cannot find C3-linearization for input"),
+            Error::BadHead => write!(f, "cannot find C3-linearization for input"),
+            Error::Circular => write!(f, "Circular dependency found"),
+        }
+    }
+}
--- projects/c3-inheritance/src/lib.rs
+++ projects/c3-inheritance/src/lib.rs
@@ -5,5 +5,7 @@
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 
 mod errors;
+mod c3;
+
 
 pub use crate::errors::{Error, Result};
